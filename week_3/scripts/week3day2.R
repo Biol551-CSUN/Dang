8e999e964cd01f4dfd70c8ac91ff62bad263e7b2
adang2011 <98852244+adang2011@users.noreply.github.com>
2022-02-10 14:43:06 -0800

    adding plot from lecture

---
+++ week_3/scripts/week3day2.R
@@ -0,0 +1,41 @@
+### Today we are going to plot penguin data ##########
+### Created by: Alex Dang ##########
+### Updated on: 2022-02-10 ################
+#############################################################
+
+#### Load libraries ############
+library(palmerpenguins)
+library(beyonce)
+library(tidyverse)
+library(here)
+
+#### Load data ################
+glimpse(penguins)
+
+### Data analysis ################
+plot1<-ggplot(data=penguins,
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) + 
+  geom_point()+
+  geom_smooth(method = "lm")+     ## method "lm" in geom_smooth makes it linear
+  labs(x = "Bill depth (mm)",
+       y = "Bill length (mm)") + 
+  scale_color_manual(values = beyonce_palette(101)) +    ### setting specific colors manually
+  theme_bw() +
+  theme(axis.title = element_text(size = 20,
+                                  color = "red"),
+        panel.background = element_rect(fill = "linen"),
+        panel.border = element_rect(fill = NA, color = "red"))
+
+
+## scale_x_continuous adjusts scales (x axis), makes sure to put "c()"
+
+## coord_flip flips the x and y axes
+## coord_fixed fix axes
+## coord_trans sets the measurements of coord ex. log10
+## coord_polar makes spiral plot
+
+### Plot output ################
+ggsave(here("week_3", "output", "penguin.png"),      ## saves the plot to output folder
+       width = 7, height = 5)   ## in inches
